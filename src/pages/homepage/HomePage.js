216242b87ff91a470466d2204f37490b6e185612
Andra <andrathema@gmail.com>
2020-02-19 18:43:38 +0100

    menu-item component

---
+++ src/pages/homepage/HomePage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Directory from '../../components/directory/Directory'
+import './HomePage.styles.scss';
+
+export const HomePage = () => (
+  <div className="homepage">
+    <Directory />
+  </div>
+)
+
