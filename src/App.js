773b0b3d2fbfa608f48580d15a4504a5f5f82af4
Andra <andrathema@gmail.com>
2020-02-22 19:11:15 +0100

    implemented firebase utils

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Header from './components/header/Header';
 import HomePage from './pages/homepage/HomePage';
 import ShopPage from './pages/shop/ShopPage';
 import SignInSignUp from './pages/sign-in-and-sign-out/SignInSignOut';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
   constructor() {
@@ -20,10 +20,22 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount(){
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user});
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if(userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
 
-      console.log(user)
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser:{
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          })
+          console.log(this.state)
+        })
+      } else {
+        this.setState({currentUser: userAuth})
+      }
     })
   }
 
--- src/pages/sign-in-and-sign-out/SignInSignOut.js
+++ src/pages/sign-in-and-sign-out/SignInSignOut.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './SignInSignOut.styles.scss';
-import SignIn from '../../components/sign-in/SignIn'
+import SignIn from '../../components/sign-in/SignIn';
+import SignUp from '../../components/sign-up/SignUp';
 
 export default function SignInSignOut() {
   return (
     <div className='sign-in-and-sign-up'>
       <SignIn/>
+      <SignUp/>
     </div>
   )
 }
