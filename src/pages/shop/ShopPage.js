2ce4e337bd9dd0446685a732b1fb4cf1e1d6397c
Andra <andrathema@gmail.com>
2020-04-22 21:31:51 +0200

    added collection overview

--- src/pages/shop/ShopPage.js
+++ src/pages/shop/ShopPage.js
@@ -1,27 +1,10 @@
-import React, { Component } from 'react'
-import CollectionPreview from '../../components/preview-collection/CollectionPreview'
-import SHOP_DATA from './ShopData';
+import React, { Component } from 'react';
 
- class ShopPage extends Component {
-   constructor(props){
-     super();
-     this.state = {
-       collections: SHOP_DATA
-     }
-   }
-  render() {
-    const {collections} = this.state;
-    return (
-      <div className='shop-page'>
-        {
-          collections.map(({id, ...otherCollectionProps}) =>(
-            <CollectionPreview key={id} {...otherCollectionProps}/>
-          ))
-        }
-        
-      </div>
-    )
-  }
-}
+import CollectionOverview from '../../components/collections-overview/collections-overview';
+
+const ShopPage = ({collections}) => 
+  (<div className='shop-page'>
+   <CollectionOverview/>
+  </div>)
 
 export default ShopPage;
