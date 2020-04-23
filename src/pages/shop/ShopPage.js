f0db80a82464daa9fa8a6161923753ad945383d2
Andra <andrathema@gmail.com>
2020-04-23 14:19:17 +0200

    created selectors and reducers fro shop data

--- src/pages/shop/ShopPage.js
+++ src/pages/shop/ShopPage.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 
 import CollectionOverview from '../../components/collections-overview/collections-overview';
+import CollectionPage from '../collection/CollectionComponent';
 
-const ShopPage = ({collections}) => 
-  (<div className='shop-page'>
-   <CollectionOverview/>
-  </div>)
+const ShopPage = ({ match }) => (
+    <div className='shop-page'>
+    <Route exact path={`${match.path}`} component={CollectionOverview}/>
+    <Route path={`${match.path}/:collectionId`} component={CollectionPage}/>
+    </div>
+);
 
 export default ShopPage;
